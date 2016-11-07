a1c9e4d5a00f8f47b9c2a9e10c22483265a6542d
Quentin_Chen <quentin.chen@foxmail.com>
2016-11-07 01:47:56 -0800

    Init.

---
+++ index.js
@@ -0,0 +1,26 @@
+var fs = require('fs'),
+  path = require('path'),
+  _ = require('underscore');
+
+var filePath = path.join(__dirname, 'template.html');
+
+function echartsMaps(args, content) {
+  var template = fs.readFileSync(filePath).toString(),
+    options    = {};
+
+  if (content.length) {
+    var options = content;
+  }
+
+  return _.template(template)({
+    id: 'echarts' + ((Math.random() * 9999) | 0),
+    option: options,
+    height: args[0] || 400,
+    width: args[1] || '85%'
+  });
+}
+
+hexo.extend.tag.register('echarts', echartsMaps, {
+  async: true,
+  ends: true
+});
