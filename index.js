0947508095098c6101ce491bd8c3e76c81242ece
Quentin_Chen <quentin.chen@foxmail.com>
2016-11-13 10:49:05 -0800

    format tidy

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 var fs = require('fs'),
-  path = require('path'),
-  _ = require('underscore');
+    path = require('path'),
+    _ = require('underscore');
 
 var filePath = path.join(__dirname, 'template.html');
 
 function echartsMaps(args, content) {
-  var template = fs.readFileSync(filePath).toString(),
-    options    = {};
+    var template = fs.readFileSync(filePath).toString(),
+        options = {};
 
-  if (content.length) {
-    var options = content;
-  }
+    if (content.length) {
+        var options = content;
+    }
 
-  return _.template(template)({
-    id: 'echarts' + ((Math.random() * 9999) | 0),
-    option: options,
-    height: args[0] || 400,
-    width: args[1] || '85%'
-  });
+    return _.template(template)({
+        id: 'echarts' + ((Math.random() * 9999) | 0),
+        option: options,
+        height: args[0] || 400,
+        width: args[1] || '85%'
+    });
 }
 
 hexo.extend.tag.register('echarts', echartsMaps, {
-  async: true,
-  ends: true
+    async: true,
+    ends: true
 });
